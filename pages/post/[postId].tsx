ee1b56dea0b8f62dae15d689b5a294e4e067f2ce
wongprom <>
2022-06-23 19:24:20 +0200

    PostPage. Create UI form for comments.

--- pages/post/[postId].tsx
+++ pages/post/[postId].tsx
@@ -1,4 +1,5 @@
 import { useQuery } from '@apollo/client'
+import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
 import React from 'react'
 import Post from '../../components/Post'
@@ -6,20 +7,40 @@ import { GET_POST_BY_POST_ID } from '../../graphql/queries'
 
 const PostPage = () => {
   const router = useRouter()
-  console.log('🚀 ~ file: [postid].tsx ~ line 9 ~ PostPage ~ router', router)
+  const { data: session } = useSession()
 
   const { data, error } = useQuery(GET_POST_BY_POST_ID, {
     variables: {
       post_id: router.query.postId,
     },
   })
-  console.log('🚀 ~ file: [postid].tsx ~ line 14 ~ PostPage ~ data', data)
 
   const post: Post = data?.getPostListByPostId
 
   return (
     <div className="mx-auto my-7 max-w-5xl">
       <Post post={post} />
+      <div className="-mt-1 rounded-b-md border border-t-0 border-gray-300 bg-white p-5 pl-16">
+        <p className="text-sm">
+          Comment as <span className="text-red-500">{session?.user?.name}</span>
+        </p>
+
+        <form className="flex flex-col space-y-2">
+          <textarea
+            disabled={!session}
+            className="h-24 rounded-md border border-gray-200 p-2 pl-4 outline-none disabled:bg-gray-50"
+            placeholder={
+              session ? `Whats you on your mind?` : 'PLease sign in to comment'
+            }
+          />
+          <button
+            type="submit"
+            className="rounded-full bg-red-500 p-3 font-semibold text-white disabled:bg-gray-200"
+          >
+            Comment
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
