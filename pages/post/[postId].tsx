646febd99738f87ba131a8fb3c1a1342e25f960c
wongprom <>
2022-07-05 16:21:24 +0200

    Disable comment button if user not logedin

--- pages/post/[postId].tsx
+++ pages/post/[postId].tsx
@@ -73,6 +73,7 @@ const PostPage = () => {
             }
           />
           <button
+            disabled={!session}
             type="submit"
             className="rounded-full bg-red-500 p-3 font-semibold text-white disabled:bg-gray-200"
           >
